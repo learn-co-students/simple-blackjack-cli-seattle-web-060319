57d494b602199d4c8257ca45a9dc07149cd5a467
Jason Leach <jlspursfan@gmail.com>
2019-05-12 23:07:02 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,6 +1,6 @@
 def welcome
   # code #welcome here
-  puts "Welcome to the Blackjack Table."
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
@@ -8,14 +8,14 @@ def deal_card
   return rand(1...11)
 end
 
-def display_card_total(card_total)
+def display_card_total(num)
   # code #display_card_total here
-  puts "Your cards add up to #{{card_total}.}"
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
   # code #prompt_user here
-  puts "Type 'h' to hit or 's' to stay."
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -23,34 +23,31 @@ def get_user_input
   gets.chomp
 end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}.  Thanks for playing!"
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
-  card1 == deal_card
-  card2 == deal_card
-  card_total = card1 + card2
-  display_card_total(card_total)
-
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
-def hit?(card_total)
+def hit?(num)
   # code hit? here
   prompt_user
-  get_user_input
+  input = get_user_input
 
-  if get_user_input == 's'
-    return card_total
-  elsif get_user_input == 'h'
+  if input == 's'
+    return num
+  elsif input == 'h'
     next_card = deal_card
-    card_total += next_card
-    return card_total
+    num += next_card
+    return num
   else
     invalid_command
-    prompt_user
+    hit?(num)
   end
 end
 
@@ -62,7 +59,14 @@ end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
-
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+
+  end_game(card_total)
 end
